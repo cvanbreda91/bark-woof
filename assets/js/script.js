13f0d74f4d24ba8a01473504eed4aaee535b81e7
Courtney Van Breda <cmvanbreda91@gmail.com>
2022-08-09 22:42:27 -0400

    test apis for functionality

---
+++ assets/js/script.js
@@ -0,0 +1,29 @@
+//personaliity API
+
+var getHashtags= function() {
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': 'ac049e0aacmsh4a22781a68935f2p12f181jsnbf8e06889ca5',
+            'X-RapidAPI-Host': 'hashtagy-generate-hashtags.p.rapidapi.com'
+        }
+    };
+    
+    fetch('https://hashtagy-generate-hashtags.p.rapidapi.com/v1/insta/tags?keyword=beagle&include_tags_info=true', options)
+        .then(response => response.json())
+        .then(response => console.log(response))
+        .catch(err => console.error(err));}
+
+var getDogs= function() {
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': 'ac049e0aacmsh4a22781a68935f2p12f181jsnbf8e06889ca5',
+            'X-RapidAPI-Host': 'dog-breeds2.p.rapidapi.com'
+        }
+    };
+    
+    fetch('https://dog-breeds2.p.rapidapi.com/dog_breeds', options)
+        .then(response => response.json())
+        .then(response => console.log(response))
+        .catch(err => console.error(err));}
