5c490a4c18b010a21fa0ad8ddfa1c3964865f8b5
Courtney Van Breda <cmvanbreda91@gmail.com>
2022-08-11 21:44:35 -0400

    dynamic api calls

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,6 @@
 //personaliity API
+var dogType ="";
+key=0;
 
 var getHashtags= function() {
     const options = {
@@ -9,7 +11,7 @@ var getHashtags= function() {
         }
     };
     
-    fetch('https://hashtagy-generate-hashtags.p.rapidapi.com/v1/insta/tags?keyword=beagle&include_tags_info=true', options)
+    fetch('https://hashtagy-generate-hashtags.p.rapidapi.com/v1/insta/tags?keyword='+dogType+'&include_tags_info=true', options)
         .then(response => response.json())
         .then(response => console.log(response))
         .catch(err => console.error(err));}
@@ -23,10 +25,21 @@ var getDogs= function() {
         }
     };
     
-    fetch('https://dog-breeds2.p.rapidapi.com/dog_breeds', options)
+    fetch('https://dog-breeds2.p.rapidapi.com/dog_breeds/breed/'+dogType, options)
         .then(response => response.json())
         .then(response => console.log(response))
         .catch(err => console.error(err));}
 
-console.log(getHashtags);
-console.log(getDogs);
+    
+
+$("#search-button").click(function(event) {
+    dogType = $("#dog-type").val();
+    key=key+1;
+    console.log(dogType);
+    localStorage.setItem('dog'+key, JSON.stringify(dogType));
+    getHashtags();
+    getDogs();
+
+    $("#buttons").append('<button id="button'+key+'">'+dogType+'</button>')
+    
+})
