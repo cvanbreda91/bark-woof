eca201f06634ce099d4580b628313df076c4b9a1
Courtney Van Breda <cmvanbreda91@gmail.com>
2022-08-12 12:24:54 -0400

    get dog image working

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,6 @@
 //personaliity API
 var dogType ="";
-var dogTypeParse=dogType.replace(/\b[a-z]/g, function(dogType) {
-    return dogType.toUpperCase();
-});
+var dogTypeParse="";
 key=0;
 
 var getHashtags= function() {
@@ -20,18 +18,37 @@ var getHashtags= function() {
         .catch(err => console.error(err));}
 
 var getDogs= function() {
-    const options = {
-        method: 'GET',
-        headers: {
-            'X-RapidAPI-Key': 'ac049e0aacmsh4a22781a68935f2p12f181jsnbf8e06889ca5',
-            'X-RapidAPI-Host': 'dog-breeds2.p.rapidapi.com'
+    var settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": "https://api.thedogapi.com/v1/breeds/search?q="+dogTypeParse,
+        "method": "GET",
+        "headers": {
+          "x-api-key": "21d2859c-1fef-4be4-9b60-202f7c47dd39"
         }
-    };
-    
-    fetch('https://dog-breeds2.p.rapidapi.com/dog_breeds/breed/'+dogTypeParse, options)
-        .then(response => response.json())
-        .then(response => console.log(response))
-        .catch(err => console.error(err));}
+      }
+      
+      $.ajax(settings).done(function (data) {
+        console.log(data);
+        var imageId = data[0].reference_image_id;
+        var settings = {
+            "async": true,
+            "crossDomain": true,
+            "url": "https://api.thedogapi.com/v1/images/"+imageId,
+            "method": "GET",
+            "headers": {
+              "x-api-key": "21d2859c-1fef-4be4-9b60-202f7c47dd39"
+            }
+          }
+          
+          $.ajax(settings).done(function (data) {
+            console.log(data);
+            var dogPicUrl = data.url;
+            console.log(dogPicUrl)
+            $("#facts-column").append('<img src="'+dogPicUrl+'" alt="'+dogTypeParse+' image"/>')
+          });
+      });
+}
 
     
 
